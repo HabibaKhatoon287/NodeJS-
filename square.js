9754a0c7f4b9a12b7263f5bc3ed5d362a0f9f1f5
HabibaKhatoon287 <habibakhatoon287@gmail.com>
2024-10-06 11:28:04 -0700

    Add files via upload

---
+++ square.js
@@ -0,0 +1,8 @@
+// exports.area = (a) =>(a*a);
+// exports.parameter = (a) =>(4*a)
+
+area = (a) =>(a*a);
+parameter = (a) =>(4*a)
+
+module.exports.area = area;
+module.exports.parameter= parameter;
