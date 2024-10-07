52f343e177f720b2f8f553c566aa6913ee1a75cd
HabibaKhatoon287 <habibakhatoon287@gmail.com>
2024-10-07 09:44:40 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-// const square = {
-//     area : (a) =>(a*a),
-//     parameter: (a) => (4*a)
-// }
+const http = require("http");
+const server = http.createServer((req, res) => {
+        res.end("hello from other side");
+})
 
-const square = require('./square.js');
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("listening to the port no 80000");
+});
 
-const calsquare = (a) => {
-    console.log(`the value of a is ${a} and the area is` +square.area(a));
-    console.log(`the value of a is ${a} and the parameter is`+square.parameter(a));
-}
 
-console.log(__filename);
-console.log(__dirname);
-calsquare(5);
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+
+// const hostname = "localhost";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     console.log('Request for ' + req.url + ' by method ' + req.method);
+
+//     if (req.method == 'GET') {
+//         let fileURL;
+
+//         if (req.url == '/') {
+//             fileURL = "index.html";
+//         } else {
+//             fileURL = req.url;
+//         }
+
+//         const filePath = path.resolve('./public' + fileURL);
+//         const fileExt = path.extname(filePath);
+
+//         if (fileExt == '.html') {
+//             fs.exists(filePath, (exists) => {
+//                 if (!exists) {
+//                     res.statusCode = 404;
+//                     res.setHeader('Content-Type', 'text/html');
+//                     res.end(`<html><body><h1>Error 404: ${fileURL} does not exist</h1></body></html>`);
+//                     return;  // Stop further execution if file does not exist
+//                 }
+
+//                 res.statusCode = 200;
+//                 res.setHeader('Content-Type', 'text/html');
+//                 fs.createReadStream(filePath).pipe(res);
+//             });
+//         } else {
+//             res.statusCode = 404;
+//             res.setHeader('Content-Type', 'text/html');
+//             res.end(`<html><body><h1>Error 404: ${fileURL} is not an HTML file</h1></body></html>`);
+//         }
+//     } else {
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end(`<html><body><h1>Error 404: ${req.url} not supported</h1></body></html>`);
+//     }
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}`);
+// });
